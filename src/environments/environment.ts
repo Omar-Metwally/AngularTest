fe95ec443f67576b924445cb74e3eac3a1127f3b
omar met <xomar.metwallyx@gmail.com>
2024-05-11 10:50:29 +0300

    2024-05-11-2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
     // appUrl: 'https://www.bl-hana.somee.com/',
-    // appUrl: 'https://taambeit.runasp.net/',
-    appUrl: 'https://localhost:7176/',
+    appUrl: 'https://taambeit.runasp.net/',
+    // appUrl: 'https://localhost:7176/',
     fireBase: 'https://firebasestorage.googleapis.com/v0/b/fir-e4e25.appspot.com/o/receipts%252F',
     userKey: 'BlHanaUser'
 };
